8e7b598544288295adc300c56b2874a9fb3a2749
Alvaro Paez <41499185+AlvaroPaez@users.noreply.github.com>
2023-06-07 00:51:22 -0500

    Delete Tareas/IGH_Tarea02_Tarjetas directory

--- Tareas/IGH_Tarea02_Tarjetas/src/Sistema/excepciones/RangoDeNvlCrediticio.java
+++
@@ -1,7 +0,0 @@
-package Sistema.excepciones;
-
-public class RangoDeNvlCrediticio extends Exception {
-    public RangoDeNvlCrediticio() {
-        super("El nivel crediticio es incorrecto. Debe estar entre 0 y 775.");
-    }
-}
